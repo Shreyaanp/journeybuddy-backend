fa22a772bd277faebf8ef0058ee56529d3e7a79b
Shreyaanp <shreyaan.pradhan2020@vitstudeent.ac.in>
2024-04-03 01:05:57 +0545

    changes made

--- main.py
+++ main.py
@@ -86,4 +86,5 @@ def qna_endpoint(data: QnaQuery):
     
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Use environment variable PORT if available, else default to 8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
